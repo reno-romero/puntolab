6f144c13905db28dcb85a27058a56255d4e86bea
reno-romero <renerome@gmail.com>
2015-07-27 13:29:56 -0500

    git inicial solo logo y animacion principal

---
+++ js/funciones.js
@@ -0,0 +1,29 @@
+ 
+$(document).ready(function(){
+
+
+	/*      BLOQUE PARA PONER Y ANIMAR PRGRESS BAR *
+	window.setTimeout(function(){
+			var progressbar = $('#barra'),
+			max = progressbar.attr('max'),
+			value = progressbar.val(),
+			time = 50;
+
+		console.log(time);
+
+		var barra_Progreso = function(){
+			value +=1;
+			addValue = progressbar.val(value);
+		}		
+		
+		setInterval(function(){
+			barra_Progreso();
+		}, time);
+	},500);
+
+	window.setTimeout(function(){
+		//$("#animacion_inicio").hide();
+	},3000);*/
+	
+	
+});
